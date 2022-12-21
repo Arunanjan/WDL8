63555bd223ee097d5223f08cba24042f19b55566
Arunanjan <93660518+Arunanjan@users.noreply.github.com>
2022-12-21 19:05:03 +0530

    Add files via upload

---
+++ models/todo.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
+};
